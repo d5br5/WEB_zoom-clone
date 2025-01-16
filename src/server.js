e4a794fdc74029eb19731fc426b3a2dcec0a6971
Doh Kim <ehgud456456@naver.com>
2025-01-16 18:34:41 +0900

    feat: socket 연결 초기화

--- src/server.js
+++ src/server.js
@@ -1,9 +1,31 @@
 import express from "express";
+import http from "http";
+import ws from "ws";
 
 const app = express();
 
-console.log("Hello World");
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use("/public", express.static(process.cwd() + "/src/public"));
 
-app.listen(3000, () => {
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => {
   console.log("Server is running on port 3000");
+};
+
+const httpServer = http.createServer(app);
+const wsServer = new ws.Server({ server: httpServer });
+
+wsServer.on("connection", (socket, req) => {
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  });
+  socket.send("hello");
+  socket.on("close", () => {
+    console.log("Disconnected from the Browser");
+  });
 });
+
+httpServer.listen(3000, handleListen);
