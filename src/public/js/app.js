e556766f55bb05df8ff3d1bc9194f2a5a63014d8
Doh Kim <ehgud456456@naver.com>
2025-01-17 15:30:06 +0900

    feat: socket.io 설치 및 연동

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,71 +1,13 @@
-// 고유 ID 생성 함수
-function generateClientId() {
-  return Math.random().toString(36).substring(2, 11);
-}
+const socket = io();
 
-// sessionStorage에서 clientId 확인 또는 생성
-function getClientId() {
-  let clientId = sessionStorage.getItem("clientId");
-  if (!clientId) {
-    clientId = generateClientId();
-    sessionStorage.setItem("clientId", clientId);
-  }
-  return clientId;
-}
+const welcome = document.querySelector("#welcome");
+const form = welcome.querySelector("form");
 
-// WebSocket 연결
-let nickname = "noname";
-const clientId = getClientId();
-
-const messageList = document.querySelector("ul");
-const messageForm = document.getElementById("message-form");
-const nicknameForm = document.getElementById("nickname-form");
-
-const socket = new WebSocket(
-  `ws://${window.location.host}?clientId=${clientId}`
-);
-
-socket.addEventListener("open", () => {
-  console.log("Connected to Server");
-});
-
-const makeMessage = (type, payload) => JSON.stringify({ type, payload });
-
-const addMessage = ({ clientId, message }) => {
-  const li = document.createElement("li");
-  li.innerText = `${clientId}: ${message}`;
-  messageList.append(li);
-};
-
-socket.addEventListener("message", (message) => {
-  const data = JSON.parse(message.data);
-  const { type } = data;
-  if (type === "history") {
-    data.payload.chatList.forEach(addMessage);
-  }
-  if (type === "message") {
-    addMessage(data);
-  }
-});
-
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server");
-});
-
-messageForm.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  const message = input.value;
-  socket.send(makeMessage("message", message));
-  addMessage({ clientId, message });
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("server is done!");
+  });
   input.value = "";
 });
-
-const handleNicknameSubmit = (event) => {
-  event.preventDefault();
-  const input = nicknameForm.querySelector("input");
-  nickname = input.value;
-  socket.send(makeMessage("nickname", nickname));
-};
-
-nicknameForm.addEventListener("submit", handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
-import ws from "ws";
+// import ws from "ws";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -8,48 +9,57 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use("/public", express.static(process.cwd() + "/src/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {});
+
+app.get("/", (_, res) => res.render("home"));
+// app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => {
   console.log("Server is running on port 3000");
 };
 
-const httpServer = http.createServer(app);
-const wsServer = new ws.Server({ server: httpServer });
-
-const browserSocketList = {};
-const chatList = [];
-
-wsServer.on("connection", (socket, req) => {
-  const urlParams = new URLSearchParams(req.url.split("?")[1]);
-  const clientId = urlParams.get("clientId") || "unknown";
-  console.log(`New connection: ${clientId}`);
-  browserSocketList[clientId] = socket;
-
-  // 브라우저 소켓 연결시 기존 채팅 목록 전송
-  socket.send(JSON.stringify({ type: "history", payload: { chatList } }));
-
-  socket.on("message", (message) => {
-    const data = JSON.parse(message.toString());
-
-    if (data.type === "message") {
-      const payload = { clientId, message: data.payload };
-      chatList.push(payload);
-      Object.entries(browserSocketList).forEach(([id, browserSocket]) => {
-        if (id !== clientId) {
-          browserSocket.send(JSON.stringify({ type: "message", ...payload }));
-        }
-      });
-    }
-
-    if (data.type === "nickname") {
-      console.log(data.payload);
-    }
-  });
-  socket.on("close", () => {
-    console.log("Disconnected from the Browser");
+io.on("connection", (socket) => {
+  console.log("Connected");
+  socket.on("enter_room", (data, done) => {
+    console.log(data);
+    done();
   });
 });
 
 httpServer.listen(3000, handleListen);
+
+// const browserSocketList = {};
+// const chatList = [];
+
+// const wsServer = new ws.Server({ server: httpServer });
+// wsServer.on("connection", (socket, req) => {
+//   const urlParams = new URLSearchParams(req.url.split("?")[1]);
+//   const clientId = urlParams.get("clientId") || "unknown";
+//   console.log(`New connection: ${clientId}`);
+//   browserSocketList[clientId] = socket;
+
+//   // 브라우저 소켓 연결시 기존 채팅 목록 전송
+//   socket.send(JSON.stringify({ type: "history", payload: { chatList } }));
+
+//   socket.on("message", (message) => {
+//     const data = JSON.parse(message.toString());
+
+//     if (data.type === "message") {
+//       const payload = { clientId, message: data.payload };
+//       chatList.push(payload);
+//       Object.entries(browserSocketList).forEach(([id, browserSocket]) => {
+//         if (id !== clientId) {
+//           browserSocket.send(JSON.stringify({ type: "message", ...payload }));
+//         }
+//       });
+//     }
+
+//     if (data.type === "nickname") {
+//       console.log(data.payload);
+//     }
+//   });
+//   socket.on("close", () => {
+//     console.log("Disconnected from the Browser");
+//   });
+// });
