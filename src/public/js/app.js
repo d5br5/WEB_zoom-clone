85430426d5f852d98385a9c7a235422c826c2b69
DohyungKim <ehgud456456@naver.com>
2025-01-19 02:33:00 +0900

    feat: 카메라 전환 버그 대응

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -83,6 +83,14 @@ const handleCameraChange = async () => {
   });
 
   await getMedia(cameraSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+
+    videoSender.replaceTrack(videoTrack);
+  }
 };
 
 cameraSelect.addEventListener("input", handleCameraChange);
