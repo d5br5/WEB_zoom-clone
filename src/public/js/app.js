ec29159e934a0fe666e5f85ca71b0ef9bdfd5032
DohyungKim <ehgud456456@naver.com>
2025-01-18 22:27:09 +0900

    feat: 카메라 전환

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,18 +3,76 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameraSelect");
+const audioSelect = document.getElementById("audioSelect");
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getMedia() {
+async function getCameras() {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    console.log(devices);
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+        option.innerText = camera.label + " (current)";
+      }
+
+      cameraSelect.appendChild(option);
     });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getAudio() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const audios = devices.filter((device) => device.kind === "audioinput");
+
+    const currentAudio = myStream.getAudioTracks()[0];
+
+    audios.forEach((audio) => {
+      const option = document.createElement("option");
+      option.value = audio.deviceId;
+      option.innerText = audio.label;
+
+      if (currentAudio.label === audio.label) {
+        option.selected = true;
+        option.innerText = audio.label + " (current)";
+      }
+
+      audioSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getMedia(deviceId) {
+  const cameraConstrains = {
+    audio: true,
+    video: deviceId
+      ? { deviceId: { exact: deviceId } }
+      : { facingMode: "user" },
+  };
+
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(cameraConstrains);
     myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+      await getAudio();
+    }
   } catch (e) {
     console.log(e);
   }
@@ -41,3 +99,13 @@ const handleCameraClick = () => {
 };
 
 cameraBtn.addEventListener("click", handleCameraClick);
+
+const handleCameraChange = async () => {
+  myStream.getVideoTracks().forEach((track) => {
+    track.stop();
+  });
+
+  await getMedia(cameraSelect.value);
+};
+
+cameraSelect.addEventListener("input", handleCameraChange);
